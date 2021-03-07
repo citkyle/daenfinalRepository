ebc74a958fa512166618d3854104d4bea4d4c983
citkyle <78676179+citkyle@users.noreply.github.com>
2021-03-06 19:09:55 -0500

    Add files via upload

--- DaenFinalQuestion1.py
+++ DaenFinalQuestion1.py
@@ -1,4 +1,4 @@
-#Print all values between 2000 and 3000 that are 
+#Print all values between 2000 and 3200 that are 
 # divisible by 7 but are not a multiple of 5
 for x in range(2000,3200,1):
     if x % 7 == 0 and x % 5 != 0:
